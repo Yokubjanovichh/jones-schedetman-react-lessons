ad1c18ea364a8f620ac7419a10401693a95ed950
yokubjanovich <yokubjanovich@gmail.com>
2023-08-29 12:08:34 +0500

    main file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { Component } from "react";
+import "./main.css";
 
-export default class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
+export default function App() {
+  return <div>app</div>;
 }
